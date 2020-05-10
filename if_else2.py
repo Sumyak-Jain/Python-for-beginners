3ac9cc1edb2c3150a9f2ccf78aa518b6ec3561a0
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-11 03:20:21 +0530

    elif

---
+++ if_else2.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number a:"))
+b=int(input("enter a number b:"))
+c=int(input("enter a number c:"))
+if a>b and a>c:
+    print("a is largest")
+elif b>a and b>c:
+    print("b is largest")
+else:
+    print("c is largest")
