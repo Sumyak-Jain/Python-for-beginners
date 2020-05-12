c7fd173e013dc17f072046e6519b92c09977cd1d
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-12 19:26:34 +0530

    list methods and functions

---
+++ list3.py
@@ -0,0 +1,21 @@
+list Function
+
+cmp(list1, list2)	It compares the elements of both the lists.
+len(list)	It is used to calculate the length of the list.
+max(list)	It returns the maximum element of the list.
+min(list)	It returns the minimum element of the list.
+list(seq)	It converts any sequence to the list.
+
+list methods
+
+list.append(obj)	The element represented by the object obj is added to the list.
+list.clear()	It removes all the elements from the list.
+List.copy()	It returns a shallow copy of the list.
+list.count(obj)	It returns the number of occurrences of the specified object in the list.
+list.extend(seq)	The sequence represented by the object seq is extended to the list.
+list.index(obj)	It returns the lowest index in the list that object appears.
+list.insert(index, obj)	The object is inserted into the list at the specified index.
+list.pop(obj=list[-1])	It removes and returns the last object of the list.
+list.remove(obj)	It removes the specified object from the list.
+list.reverse()	It reverses the list.
+list.sort([func])	It sorts the list by using the specified compare function if given.
