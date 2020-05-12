240288bb7c43c3b5473746146ed3a8d7ba6152b9
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-12 17:31:14 +0530

    String methods

---
+++ String1.py
@@ -0,0 +1,14 @@
+various methods in String
+
+capitalize()
+casefold()
+count(string,begin,end)
+find(substring ,beginIndex, endIndex)
+index(subsring, beginIndex, endIndex)
+isupper()
+join(seq)
+len(string)
+lower()
+partition()
+split(str,num=string.count(str))
+upper()
