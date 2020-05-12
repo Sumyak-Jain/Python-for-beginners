310c98d3f25862815b95315ae1bba33842031983
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-12 23:56:24 +0530

    remove element

---
+++ set2.py
@@ -0,0 +1,9 @@
+set1={"gaurav0","gaurav1","gaurav2","gaurav3","gaurav4"}
+print(set1)
+
+set1.add("jain")
+print(set1)
+set1.remove("gaurav0") #type1
+set1.discard("gaurav1") #type2
+set1.pop() #type3
+print(set1)
