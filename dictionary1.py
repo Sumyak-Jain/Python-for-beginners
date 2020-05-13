ba3dfe850e0f1ecf61d7a28de9cd2a3a58bb4c82
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-13 16:36:39 +0530

    updating dictionary

---
+++ dictionary1.py
@@ -0,0 +1,6 @@
+student={"name":"gaurav","branch":"devops","roll no.":106}
+print(student)
+student["name"]=input("enter name")
+student["branch"]=input("enter branch")
+student["roll no."]=input("enter roll no.")
+print(student)
